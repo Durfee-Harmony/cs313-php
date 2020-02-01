c6e3ae2f427f2c2ae4d6e6ed3d32f9ffb1029f99
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-01 16:55:44 -0700

    shopping

--- web/assignments/week3/shop.php
+++ web/assignments/week3/shop.php
@@ -2,12 +2,12 @@
 
 function browse()
 {
-  $products = array("anvil", "mallet", "piano", "rope", "trap");
+  $products = array(0 => "anvil", 1 => "mallet", 2 => "piano", 3 => "rope", 4 => "trap");
   $browse = '<div id="products">';
+  $browse .= "<h1>Products:</h1>";
   for($i=0; $i<5; $i++){
     $browse .= '<img src="../../media/products/' . $products[$i] . '.jpg" alt="' . $products[$i] . '" width="100px"';
   }
-  $browse .= "<h1>Products:</h1>";
   $browse .= '</div>';
   return $browse;
 }
