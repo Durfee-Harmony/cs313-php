ff8babe81135d8672fe6cf291cb4aeb94738c3b3
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-01 16:59:25 -0700

    shopping

--- web/assignments/week3/shop.php
+++ web/assignments/week3/shop.php
@@ -3,18 +3,15 @@
 function browse()
 {
   $products = array(0 => "anvil", 1 => "mallet", 2 => "piano", 3 => "rope", 4 => "trap");
+  var_dump($products);
   $browse = '<div id="products">';
   $browse .= "<h1>Products:</h1>";
   for($i=0; $i<5; $i++){
     $browse .= '<img src="../../media/products/' . $products[$i] . '.jpg" alt="' . $products[$i] . '" width="100px"';
+    var_dump($i);
   }
   $browse .= '</div>';
   return $browse;
 }
-
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-  $action = filter_input(INPUT_GET, 'action');
-};
 
 include 'shopping.php';
