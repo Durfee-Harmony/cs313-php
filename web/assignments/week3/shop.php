71fdb747a2b90d1f83d26c669ccbbf457cae8002
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-01 17:48:43 -0700

    shopping

--- web/assignments/week3/shop.php
+++ web/assignments/week3/shop.php
@@ -1,4 +1,6 @@
 <?php
+
+echo '1';
 // $action = filter_input(INPUT_POST, 'action');
 // if ($action == NULL) {
 //   $action = filter_input(INPUT_GET, 'action');
@@ -8,6 +10,7 @@ $shop;
 $check = NULL;
 $cart = '<div id="cart"><h1>Cart:</h1>';
 
+echo '2';
 function browse() {
   $products = array(0 => "anvil", 1 => "mallet", 2 => "piano", 3 => "rope", 4 => "trap");
   $browse = '<div id="products">';
@@ -20,6 +23,7 @@ function browse() {
   return $browse;
 }
 
+echo '3';
 function add($product) {
   global $cart;
   $cart .= '<img src="../../media/products/' . $product . '.jpg" alt="' . $product . '" width="100px">';
@@ -28,6 +32,7 @@ function add($product) {
   var_dump($cart);
 }
 
+echo '4';
 function remove($product) {
   global $cart;
   $cart -= '<img src="../../media/products/' . $product . '.jpg" alt="' . $product . '" width="100px">';
@@ -36,6 +41,7 @@ function remove($product) {
   var_dump($cart);
 }
 
+echo '5';
 if ($action == "add") {
   $item = filter_input(INPUT_POST, 'item', FILTER_SANITIZE_STRING);
   add($item);
@@ -46,15 +52,19 @@ if ($action == "add") {
 
 $cart .= '</div>';
 
+echo '6';
 function cart() {
   global $cart, $check;
   $check = 1;
   $shop = $cart;
+  echo '7';
   include 'shopping.php';
 }
 
+echo '8';
 if ($check == NULL) {
   $shop = browse();
+  echo '9';
   include 'shopping.php';
   echo 'check';
 }
