5ed02a9fe6115d5775deb26933da3bbc9d14d3cc
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-01 18:19:32 -0700

    shopping

--- web/assignments/week3/shop.php
+++ web/assignments/week3/shop.php
@@ -3,7 +3,6 @@ $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
   $action = filter_input(INPUT_GET, 'action');
 }
-$action = NULL;
 $shop;
 $check = NULL;
 $cart = '<div id="cart"><h1>Cart:</h1>';
