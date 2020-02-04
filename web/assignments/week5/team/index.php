b616b0c938309ccfb910d387e05a672191c5c52c
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-04 16:23:51 -0700

    update

---
+++ web/assignments/week5/team/index.php
@@ -0,0 +1,17 @@
+<?php
+require "dbConnect.php";
+$db = get_db();
+
+$scriptures = $db->prepare("SELECT * FROM w5_scriptures");
+$scriptures->execute();
+
+echo "<h1>Scripture Resources</h1>";
+
+while ($fRow = $scriptures->fetch(PDO::FETCH_ASSOC)){
+  $book = $fRow["book"];
+  $chapter = $fRow["chapter"];
+  $verse = $fRow["verse"];
+  $content = $fRow["content"];
+
+  echo '<p><strong>$book $chapter:$verse</strong> &ndash; "$content"</p>';
+}
