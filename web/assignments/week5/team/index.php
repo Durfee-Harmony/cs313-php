339a8ce551788bfec22021df828ffae2766cdd3b
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-06 14:57:58 -0700

    update

--- web/assignments/week5/team/index.php
+++ web/assignments/week5/team/index.php
@@ -7,13 +7,31 @@ $scriptures->execute();
 
 echo "<h1>Scripture Resources</h1>";
 
-while ($row = $scriptures->fetch(PDO::FETCH_ASSOC)){
+while ($row = $scriptures->fetch(PDO::FETCH_ASSOC)) {
   $book = $row["book"];
   $chapter = $row["chapter"];
   $verse = $row["verse"];
   $content = $row["content"];
   $id = $row["id"];
-
-  echo "this worked";
   echo "<p><a href='detail.php?id=$id'><strong>$book $chapter:$verse</strong></a></p>";
+}
+
+echo "<br><br><h3>Search for Scripture by Book:</h3>";
+echo "<form method='post'><input type='text' name='scriptureSearch'>";
+echo "<button type='submit' name='search' value='Search'>Search</button></form>";
+
+if (isset($_POST['search'])) {
+  $searchString = $_POST['scriptureSearch'];
+  strtolower($searchString);
+  ucfirst($searchString);
+  $scriptureData = $db->prepare("SELECT * FROM w5_scriptures WHERE book = '$searchString'");
+  $scriptureData->execute();
+  while ($row = $scriptureData->fetch(PDO::FETCH_ASSOC)) {
+    $book = $row['book'];
+    $chapter = $row['chapter'];
+    $verse = $row['verse'];
+    $content = $row['content'];
+
+    echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+  }
 }
