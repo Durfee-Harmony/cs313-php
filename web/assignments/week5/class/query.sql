624840ca1c4e8948a2c1b09212d153ffede7264e
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-06 16:59:41 -0700

    update

--- web/assignments/week5/class/query.sql
+++ web/assignments/week5/class/query.sql
@@ -1,29 +1,39 @@
-\echo '2a. How many events are there?'
----------------- Your code here ----------------
+-- \echo '2a. How many events are there?'
+-- SELECT COUNT(id) FROM w5_EVENT;
 
 -- \echo '2b. How many participants are there?'
----------------- Your code here ----------------
+-- SELECT COUNT(id) FROM w5_PARTICIPANT;
 
 -- \echo '3a. What is the third event when sorted alphabetically (by name)? '
----------------- Your code here ----------------
+-- SELECT name FROM w5_EVENT ORDER BY name LIMIT (1) OFFSET (2);
 
 -- \echo '3b. What is the third event when sorted by ID? '
----------------- Your code here ----------------
+-- SELECT id, name FROM w5_EVENT ORDER BY id LIMIT (1) OFFSET (2);
 
 -- \echo '4a. List the names alphabetically of all the participants in the ''Toughman Utah'' competition'
----------------- Your code here ----------------
+-- SELECT ep.participant_id, p.id, p.name
+--   FROM w5_EVENT_PARTICIPANT ep
+--     JOIN w5_PARTICIPANT p ON p.id = ep.participant_id
+--   WHERE ep.event_id = 5
+-- ORDER BY p.name;
 
 -- \echo '4b. List the names (sorted by id) of all the participants in the ''Kauai Marathon'' competition'
----------------- Your code here ----------------
+-- SELECT ep.participant_id, p.id, p.name
+--   FROM w5_EVENT_PARTICIPANT ep
+--     JOIN w5_PARTICIPANT p ON p.id = ep.participant_id
+--   WHERE ep.event_id = 11
+-- ORDER BY p.id;
 
 -- \echo '5a. How many competitions has ''Black Panther'' competed in?'
----------------- Your code here ----------------
+-- SELECT COUNT(event_id) FROM w5_EVENT_PARTICIPANT WHERE participant_id = 32;
 
--- \echo '5b. How can you verify that your count is correct?'  
----------------- Your code here ----------------
+-- \echo '5b. How can you verify that your count is correct?'
+-- SELECT id, name FROM w5_PARTICIPANT WHERE name = 'Black Panther';
+-- SELECT id, event_id, participant_id FROM w5_EVENT_PARTICIPANT WHERE participant_id = 32;
 
 -- \echo '5c. Who has competed in the most competitions? How many have they competed in?'
----------------- Your code here ----------------
+-- SELECT COUNT(ep.event_id) FROM w5_EVENT_PARTICIPANT ep
+--   JOIN w5_PARTICIPANT p ON p.id = ep.participant_id;
 
 -- \echo '5d. Who has competed in the least competitions? How many have they competed in?'
 ---------------- Your code here ----------------
