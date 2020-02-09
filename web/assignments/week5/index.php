ce47f9af7c1e45d5f529e1eee529ec88ece7c6a0
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-08 22:36:26 -0700

    update

--- web/assignments/week5/index.php
+++ web/assignments/week5/index.php
@@ -7,7 +7,7 @@ FROM quote q
   JOIN author_quote aq ON aq.quote_id = q.id
   JOIN author a ON a.id = aq.author_id
   JOIN quote_category qc ON q.id = qc.quote_id
-  JOIN category c ON c.id = qc.category_id;");
+  JOIN category c ON c.id = qc.category_id");
 $quotes->execute();
 
 echo "<h1>Quotes:</h1>";
