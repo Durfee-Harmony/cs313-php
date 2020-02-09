71fecaf945b506c80aa35d9d0794b74974482241
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-08 22:21:40 -0700

    update

--- web/assignments/week5/index.php
+++ web/assignments/week5/index.php
@@ -2,10 +2,10 @@
 require_once "team/dbConnect.php";
 $db = get_db();
 
-$quotes = $db->prepare("SELECT q.txt, a.name
-                        FROM quote q
-                          JOIN author_quote aq ON aq.quote_id = q.id
-                          JOIN author a ON a.id = aq.author_id");
+$quotes = $db->prepare("SELECT quote.txt, author.name
+                        FROM quote
+                          JOIN author_quote ON author_quote.quote_id = quote.id
+                          JOIN author ON author.id = author_quote.author_id");
 $quotes->execute();
 
 echo "<h1>Quotes:</h1>";
