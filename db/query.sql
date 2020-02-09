745cbbe73219208b331bc18f89c156e4b01fe2cd
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-08 22:29:38 -0700

    update

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,6 @@
-SELECT q.txt, a.name
+SELECT a.name AS "author", q.txt AS "quote", c.name AS "category"
 FROM quote q
   JOIN author_quote aq ON aq.quote_id = q.id
-  JOIN author a ON a.id = aq.author_id;
+  JOIN author a ON a.id = aq.author_id
+  JOIN quote_category qc ON q.id = qc.quote_id
+  JOIN category c ON c.id = qc.category_id;
--- web/assignments/week5/index.php
+++ web/assignments/week5/index.php
@@ -2,17 +2,21 @@
 require_once "team/dbConnect.php";
 $db = get_db();
 
-$quotes = $db->prepare("SELECT quote.txt, author.name
-                        FROM quote
-                          JOIN author_quote ON author_quote.quote_id = quote.id
-                          JOIN author ON author.id = author_quote.author_id");
+$quotes = $db->prepare("SELECT a.name AS \"author\", q.txt AS \"quote\", c.name AS \"category\"
+FROM quote q
+  JOIN author_quote aq ON aq.quote_id = q.id
+  JOIN author a ON a.id = aq.author_id
+  JOIN quote_category qc ON q.id = qc.quote_id
+  JOIN category c ON c.id = qc.category_id;");
 $quotes->execute();
 
 echo "<h1>Quotes:</h1>";
 
 while ($row = $quotes->fetch(PDO::FETCH_ASSOC)) {
-  $author = $row["a.name"];
-  $quote = $row["q.txt"];
+  $author = $row["author.name"];
+  $quote = $row["quote.txt"];
+  $cat = $row["category.name"];
   echo "<h3>$author</h3>";
   echo "<p>\"$quote\"</p>";
+  echo "<p>$cat</p>";
 }
