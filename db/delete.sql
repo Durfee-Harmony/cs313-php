221d630480101e3f0360c43fb0b4ace21a9c0b62
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-08 22:56:57 -0700

    update

--- db/delete.sql
+++ db/delete.sql
@@ -1,5 +1,5 @@
-DROP TABLE author;
-DROP TABLE quote;
-DROP TABLE category;
-DROP TABLE author_quote;
-DROP TABLE quote_category;
+DROP TABLE author CASCADE;
+DROP TABLE quote CASCADE;
+DROP TABLE category CASCADE;
+DROP TABLE author_quote CASCADE;
+DROP TABLE quote_category CASCADE;
