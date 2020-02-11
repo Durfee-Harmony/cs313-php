adf969049534f6cbddbb032db3166aa4651bc248
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-11 15:54:28 -0700

    update

--- web/assignments/week6/tuesday/display.php
+++ web/assignments/week6/tuesday/display.php
@@ -6,7 +6,7 @@
 		<div class="container">
          <?php
             $personId = $_GET['personId'];
-            $statement = $db->prepare('SELECT * FROM w5_user WHERE ID = :personId');
+            $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
             $statement->bindValue(':personId', $personId);
             $statement->execute();
             while($row = $statement->fetch(PDO::FETCH_ASSOC)){
@@ -15,10 +15,10 @@
                $last = $row['last_name'];
                $food_id = $row['food_type'];
       
-               $state = $db->prepare('SELECT * FROM w5_food WHERE ID = $food_id');
+               $state = $db->prepare("SELECT * FROM w6_food WHERE ID = $food_id");
                $state->execute();
-               while($row = $state->fetch(PDO::FETCH_ASSOC)){
-                  $food = $row['food'];
+               while($fRow = $state->fetch(PDO::FETCH_ASSOC)){
+                  $food = $fRow['food'];
                }
                echo "<h1>$first $last's favorite food is $food</h1>";
             }
