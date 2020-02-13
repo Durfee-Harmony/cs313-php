f208c006182324bad10a1327640e5dfd74d86122
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-13 15:36:11 -0700

    update

---
+++ web/project1/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once "dbConnect.php";
+$db = get_db();
+
+$quotes = $db->prepare("SELECT *
+FROM quote q
+  JOIN author_quote aq ON aq.quote_id = q.id
+  JOIN author a ON a.id = aq.author_id
+  JOIN quote_category qc ON q.id = qc.quote_id
+  JOIN category c ON c.id = qc.category_id");
+$quotes->execute();
+
+echo "<h1>Quotes:</h1>";
+
+while ($row = $quotes->fetch(PDO::FETCH_ASSOC)) {
+  $author = $row["name"];
+  $quote = $row["txt"];
+  $cat = $row["cat"];
+  echo "<br><h3>Author: $author</h3>";
+  echo "<p>\"$quote\"</p>";
+  echo "<p><strong>Category: $cat</strong></p>";
+}
