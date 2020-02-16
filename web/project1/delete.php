a8d8e098b5522d39ce40b7b55dc0b9013f4715e9
Durfee-Harmony <harmonydurfee@gmail.com>
2020-02-15 17:49:36 -0700

    update

--- web/project1/delete.php
+++ web/project1/delete.php
@@ -1,3 +1,27 @@
 <?php
 require_once "dbConnect.php";
 $db = get_db();
+
+$quotes = $db->prepare("SELECT *
+FROM quote q
+  JOIN author_quote aq ON aq.quote_id = q.id
+  JOIN author a ON a.id = aq.author_id
+  JOIN quote_category qc ON q.id = qc.quote_id
+  JOIN category c ON c.id = qc.category_id
+WHERE q.id = :id");
+$quotes->bindValue(':id', $id);
+$quotes->execute();
+
+$page = "";
+while ($row = $quotes->fetch(PDO::FETCH_ASSOC)) {
+  $author = $row["name"];
+  $quote = $row["txt"];
+  $cat = $row["cat"];
+
+  $page .= "<br><h3>Author: $author</h3>";
+  $page .= "<p>\"$quote\"</p>";
+  $page .= "<p><strong>Category: $cat</strong></p>";
+  $page .= "<a class='button' href='update.php?id=$id'>Update</a>";
+}
+
+include "detail.php";
--- web/project1/insert.php
+++ web/project1/insert.php
@@ -8,24 +8,27 @@ if ($x == NULL) {
 	$x = filter_input(INPUT_GET, 'x');
 }
 
+$page = "";
 if ($x == 'q') {
-	echo "<form method='post' action='data.php?i=q'><div>";
-	echo "<p>Enter Quote Text: </p><input type='text' name='txt' id='txt' required>";
-	echo "<p>Choose Quote Author: </p>";
+	$page .= "<form method='post' action='data.php?i=q'><div>";
+	$page .= "<p>Enter Quote Text: </p><input type='text' name='txt' id='txt' required>";
+	$page .= "<p>Choose Quote Author: </p>";
 	$a = authors($db);
-	echo $a;
-	echo "<p>Choose Quote Category: </p>";
+	$page .= $a;
+	$page .= "<p>Choose Quote Category: </p>";
 	$c = categories($db);
-	echo $c;
-	echo "</div><br><input type='submit' class='button' name='submit' value='Add Quote'>";
-	echo "<input type='hidden' name='i' value='q'></form>";
-	echo "quote";
+	$page .= $c;
+	$page .= "</div><br><input type='submit' class='button' name='submit' value='Add Quote'>";
+	$page .= "<input type='hidden' name='i' value='q'></form>";
+	$page .= "quote";
 } else if ($x == 'a') {
-	echo "author";
+	$page .= "author";
 } else if ($x == 'c') {
-	echo "category";
+	$page .= "category";
 } else {
-	echo "<a class='button' href='insert.php?x=q'>Add Quote</a>";
-	echo "<a class='button' href='insert.php?x=a'>Add Author</a>";
-	echo "<a class='button' href='insert.php?x=c'>Add Category</a>";
+	$page .= "<a class='button' href='insert.php?x=q'>Add Quote</a>";
+	// $page .= "<a class='button' href='insert.php?x=a'>Add Author</a>";
+	// $page .= "<a class='button' href='insert.php?x=c'>Add Category</a>";
 }
+
+include "detail.php";
