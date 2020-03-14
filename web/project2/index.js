46bc1d6a4f645fd46a96c63a91124878a127e9cb
Durfee-Harmony <harmonydurfee@gmail.com>
2020-03-14 12:28:24 -0600

    update

--- web/project2/index.js
+++ web/project2/index.js
@@ -1,28 +1,23 @@
 console.log('index.js running!');
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.set('view engine', 'ejs');
 app.listen(8080);
 app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/quotes', function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  var student = {
-    name: 'Harmony',
-    class: 'cs313'
-  }
-  res.end(JSON.stringify(student));
-  res.render('quotes');
-});
-
+var results;
 const initOptions = {
   query(e) {
     console.log('QUERY:', e.query);
+  },
+  receive(data, result, e) {
+    results = camelizeColumns(data);
+    console.log('DATA:', results);
+    results = camelizeColumns(result);
+    console.log('RESULT:', results);
   }
 };
 const pgp = require('pg-promise')(initOptions);
@@ -33,15 +28,37 @@ var connect = {
   rejectUnauthorized: true
 }
 const db = pgp(connect);
-db.one('SELECT txt FROM quote WHERE id = 1')
-  .then(function (data) {
-    console.log('DATA:', data.value)
-  })
+db.one('SELECT COUNT(*) FROM quote')
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  });
+db.any('SELECT * FROM quote')
   .catch(function (error) {
     console.log('ERROR:', error)
   });
 
-// postgres://lpdzdeczvntfek:c870d329c80fb6b49f55f425360e16bc9465fb10de5601ff67a60b61abe900f8@ec2-34-193-42-173.compute-1.amazonaws.com:5432/d3b0o3nhe1tsra
+function camelizeColumns(data) {
+  const tmp = data[0];
+  for (const prop in tmp) {
+    const camel = pgp.utils.camelize(prop);
+    if (!(camel in tmp)) {
+      for (let i = 0; i < data.length; i++) {
+        const d = data[i];
+        d[camel] = d[prop];
+        delete d[prop];
+      }
+    }
+  }
+  return tmp;
+}
+
+app.get('/quotes', function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(JSON.stringify(results));
+  res.render('quotes');
+});
 
 // app.get('/detail', function (req, res) {
 //   res.send('id: ' + req.query.id);
